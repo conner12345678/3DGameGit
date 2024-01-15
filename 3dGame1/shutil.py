07c781f925c5d885e1fc2adfe8b0c343d7509d99
conner12345678 <cehly065@west-mec.org>
2024-01-15 14:01:41 -0700

    stuff
    stuff

---
+++ 3dGame1/shutil.py
@@ -0,0 +1,4 @@
+import shutil
+shutil.copy('source_file.txt', 'destination_file.txt')
+shutil.copytree('source_directory', 'destination_directory')
+shutil.move('old_file.txt', 'new_location/new_file.txt')
